c9ea5854a3a650493012573c7685d878b18f7ff0
mahieddine.ichir@gmail.com <mahieddine.ichir@gmail.com@288d3290-07ba-3f9b-fcf9-7d66c8fef408>
2013-12-04 16:09:00 +0000

    Initial import.

---
+++ trunk/src/main/webapp/WEB-INF/js/todo.js
@@ -0,0 +1,11 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: michir
+ * Date: 13/02/13
+ * Time: 14:10
+ * To change this template use File | Settings | File Templates.
+ */
+
+function Todo($routeParams) {
+
+}
