9e7d2fadf1bd336f2714f3584e8954cb1651af8d
mahieddine.ichir <mahieddine.ichir@gmail.com>
2013-12-05 10:18:13 +0000

    + welcome home page: calendar + layout js, css and img files and adding jstl tag to access static resources

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,18 +1,9 @@
-/**
- * Created with JetBrains WebStorm.
- * User: michir
- * Date: 13/02/13
- * Time: 11:50
- * To change this template use File | Settings | File Templates.
- */
-
 'use strict';
-
 angular.module('calendar', [])
     .config(['$routeProvider', function($routeProvider) {
         $routeProvider
             .when('/calendar', {templateUrl: 'views/calendar.html', controller: Calendar})
-            .when('/calendar/todo', {templateUrl: 'views/todo.html', controller: Todo})
+            .when('/todo', {templateUrl: 'views/todo.html', controller: Todo})
             .otherwise({redirectTo: '/calendar'})
         ;
 }]);
