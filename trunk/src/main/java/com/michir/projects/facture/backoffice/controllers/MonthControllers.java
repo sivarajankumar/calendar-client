aa9e03426fa56790fdf82e5f102f09415e2f4580
mahieddine.ichir@gmail.com <mahieddine.ichir@gmail.com@288d3290-07ba-3f9b-fcf9-7d66c8fef408>
2013-12-23 17:16:46 +0000

    + Adding "Acces-Control-Origin" to responses

--- trunk/src/main/java/com/michir/projects/facture/backoffice/controllers/MonthControllers.java
+++ trunk/src/main/java/com/michir/projects/facture/backoffice/controllers/MonthControllers.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,7 +20,10 @@ import com.michir.projects.facture.backoffice.data.Day;
 public class MonthControllers {
 
 	@RequestMapping(value="/{month}", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
-	public @ResponseBody Map<String, Object> getDays(@PathVariable("month") Integer month) {
+	public @ResponseBody Map<String, Object> getDays(@PathVariable("month") Integer month, HttpServletResponse response) {
+		
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		
 		Map<String, Object> map = new HashMap<String, Object>();
 		map.put("days", Arrays.asList(new Day("Lundi")));
 		return map;
