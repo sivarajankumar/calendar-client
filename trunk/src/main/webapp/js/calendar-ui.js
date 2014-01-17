a95b1c320973652c085a8c605e7461ca08e8f2ae
mahieddine.ichir@gmail.com <mahieddine.ichir@gmail.com@288d3290-07ba-3f9b-fcf9-7d66c8fef408>
2014-01-17 20:45:56 +0000

    Adding entries view

--- trunk/src/main/webapp/js/calendar-ui.js
+++ trunk/src/main/webapp/js/calendar-ui.js
@@ -1,3 +1,31 @@
+var months = [
+	                 {name: "jan.", index:0},
+	                 {name: "feb.", index:1},
+	                 {name: "mar.", index:2},
+	                 {name: "apr.", index:3},
+	                 {name: "may.", index:4},
+	                 {name: "jun.", index:5},
+	                 {name: "jul.", index:6},
+	                 {name: "aug.", index:7},
+	                 {name: "sep.", index:8},
+	                 {name: "oct.", index:9},
+	                 {name: "nov.", index:10},
+	                 {name: "dec.", index:11}
+	                 ];
+
+/**
+ * Reference data: day name, day index on ui-table (<td />), day index in fetched data.      
+ */  
+var ref = [
+           {name:"lun.", index_ui:0, index_data:2},
+           {name:"mar.", index_ui:1, index_data:3},
+           {name:"mer.", index_ui:2, index_data:4},
+           {name:"jeu.", index_ui:3, index_data:5},
+           {name:"ven.", index_ui:4, index_data:6},
+           {name:"sam.", index_ui:5, index_data:7},
+           {name:"dim.", index_ui:6, index_data:1}
+           ];
+
 /**
  * Checks whether a day array contains (name property) contains a day with name dayname.
  */     
@@ -55,18 +83,6 @@ function sortWeekDays(week) {
 	});
 };
 
-/**
- * Reference data: day name, day index on ui-table (<td />), day index in fetched data.      
- */  
-var ref = [
-           {name:"lun.", index_ui:0, index_data:2},
-           {name:"mar.", index_ui:1, index_data:3},
-           {name:"mer.", index_ui:2, index_data:4},
-           {name:"jeu.", index_ui:3, index_data:5},
-           {name:"ven.", index_ui:4, index_data:6},
-           {name:"sam.", index_ui:5, index_data:7},
-           {name:"dim.", index_ui:6, index_data:1}
-           ];
 
 /**
  * Fill week with referenced (empty) data for missing days (days not in this month)
